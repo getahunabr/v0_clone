9e4017d911d6c3343d3ab54d3fe6c68b6383b327
Sintayehu <sentflix@gmail.com>
2026-08-08 21:58:29 +0300

    05 Home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,35 @@
+import ProjectForm from "@/modules/home/components/project-form";
 import Image from "next/image";
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
+import React from "react";
 
-export default function Home() {
+const page = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <Button>Test</Button>
+    <div className="flex items-center justify-center w-full px-4 py-8">
+      <div className="max-w-5xl w-full">
+        <section className="space-y-8 flex flex-col items-center">
+          <div className="flex flex-col items-center">
+            <Image
+              src={"/logo.svg"}
+              alt="logo"
+              width={100}
+              height={100}
+              className="hidden md:block invert dark:invert-0"
+            />
+          </div>
+          <h1 className="text-2xl md:text-5xl font-bold text-center">
+            Build Something you ❤️{" "}
+          </h1>
+          <p className="text-lg md:text-xl text-muted-foreground text-center">
+            Create Apps and websites by chatting with AI
+          </p>
+
+          <div className="max-w-3xl w-full">
+            <ProjectForm />
+          </div>
+        </section>
+      </div>
     </div>
   );
-}
+};
+
+export default page;
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/theme-provider";
 
 import "./globals.css";
+import { Toaster } from "@/components/ui/toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html
         lang="en"
+        suppressHydrationWarning
         className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
       >
         <body className="min-h-full flex flex-col">
@@ -38,6 +40,7 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
+            <Toaster />
             {children}
           </ThemeProvider>
         </body>
